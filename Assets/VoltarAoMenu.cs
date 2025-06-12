5b12dce027cead8c0882264622cf56f933dd5947
Brunelli12 <eduardobrunelli12@gmail.com>
2025-06-12 07:36:47 -0300

    menupronto

--- Assets/VoltarAoMenu.cs
+++ Assets/VoltarAoMenu.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class VoltarAoMenu
+public class VoltarAoMenu : MonoBehaviour
 {
-    
+    public void VoltarMenu()
+    {
+        SceneManager.LoadScene("Menu 1"); // Troque "Menu" pelo nome exato da sua cena do menu
+    }
 }
